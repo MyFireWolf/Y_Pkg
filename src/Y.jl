6878406f7449ef8e4149b36f462374c6ba65f36f
YXT <522828368@qq.com>
2022-11-19 18:55:50 +0800

    Update Y.jl

--- src/Y.jl
+++ src/Y.jl
@@ -1,23 +1,16 @@
 module Y
-#= 
-using Base64   
-         需要使用Base64模块，则编辑 当前包文件夹的Project.toml文件：
-         添加如下内容
+#= using Base64    如果需要使用Base64模块，
+      则编辑 当前包文件夹的Project.toml文件：  添加如下内容
                 [deps] 
                 Base64 = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
 =#
-
 using GLFW
 export
     showwindows
 
 function showwindows()
-    # Create a window and its OpenGL context
     window = GLFW.CreateWindow(640, 480, "GLFW.jl")
-
-    # Make the window's context current
     GLFW.MakeContextCurrent(window)
-
     # Loop until the user closes the window
     while !GLFW.WindowShouldClose(window)
 
